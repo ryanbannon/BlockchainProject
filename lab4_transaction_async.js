96234ab7598356e20c2fc5a1daec0c50ceec1697
EC2 Default User <ec2-user@ip-172-31-14-43.ec2.internal>
2021-04-14 21:30:27 +0000

    added infura url and secret keys of accounts

--- lab4_transaction_async.js
+++ lab4_transaction_async.js
@@ -1,17 +1,18 @@
+const keys = require('./keys.js')
 const Tx = require('ethereumjs-tx').Transaction
 
 const Web3 = require('web3')
 
-const web3 = new Web3("https://ropsten.infura.io/v3/1e86eba9862b4b78b37cfaf9675ffcad")
+const web3 = new Web3(keys.web3Infura);
 
-const account1 = "0x1cF88d2952A6d7FB2f6F9E76BEB2A7904c9504BA"  // this is the address of account 1 - this guy has all the MONEH
-const account2 = "0x3d6d5A49d89eC0760E3607b287aE7693FA8D0c9A"  // this is the address of account 2 - this guy has very little MONEH
+const account1 = keys.account1; // this is the address of account 1 - this guy has all the MONEH
+const account2 = keys.account2; // this is the address of account 2 - this guy has very little MONEH
 
-const privateKey1 = Buffer.from('...', 'hex')
-const privateKey2 = Buffer.from('...', 'hex')
+const privateKey1 = Buffer.from(keys.privateKey1, 'hex');
+const privateKey2 = Buffer.from(keys.privateKey2, 'hex');
 
-//secp256k1 (elliptic curve - 256bits)
-//metamask seed phrase (12 words) - > BIP -> create 128bits of randomness
+// secp256k1 (elliptic curve - 256bits)
+// metamask seed phrase (12 words) - > BIP -> create 128bits of randomness
 // passworrd is local only, used to encrypt the seed phrase
 // derivation path: m/44’/60’/0’/0/1 -> make the same public/private keypair
 // public key -> hashed/chopped -> eth address
